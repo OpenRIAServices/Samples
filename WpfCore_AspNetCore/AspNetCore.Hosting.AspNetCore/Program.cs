6c30c62b85dc7202220042f712eb394e2ce2088b
Daniel Svensson <daniel.svensson@hotmail.se>
2023-10-20 23:36:04 +0200

    Start adding a authentication service

--- WpfCore_AspNetCore/AspNetCore.Hosting.AspNetCore/Program.cs
+++ WpfCore_AspNetCore/AspNetCore.Hosting.AspNetCore/Program.cs
@@ -1,3 +1,5 @@
+using AspNetCore.Hosting.AspNetCore.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using OpenRiaServices.Hosting.AspNetCore;
 using SimpleApplication.Web;
 
@@ -6,14 +8,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Register AddOpenRiaServices and all DomainServices
 builder.Services.AddOpenRiaServices();
 builder.Services.AddTransient<SampleDomainService>();
+builder.Services.AddTransient<MyAuthenticationService>();
+
+// Additional dependencies for cookie based AuthenticationService
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
+
+// Add authentication
+app.UseAuthentication();
+// TODO: app.UseAuthorization();
+
 app.MapGet("/", () => "Hello Open Ria Services !\n\nNow you can start the client and call the Sample service");
 
 // Enable mapping of all requests to root 
 app.MapOpenRiaServices(builder =>
 {
 	builder.AddDomainService<SampleDomainService>();
+    builder.AddDomainService<MyAuthenticationService>();
 });
 
 app.Run();
---
+++ WpfCore_AspNetCore/AspNetCore.Hosting.AspNetCore/Services/AuthenticationService.cs
@@ -0,0 +1,105 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using OpenRiaServices.Server;
+using OpenRiaServices.Server.Authentication;
+
+namespace AspNetCore.Hosting.AspNetCore.Services
+{
+    public class MyUser : IUser
+    {
+        [Key]
+        public string Name { get; set; }
+        public IEnumerable<string> Roles { get; set; } = new List<string>();
+    }
+
+    [EnableClientAccess]
+    [Authorize]
+    public class MyAuthenticationService : DomainService, IAuthentication<MyUser>
+    {
+        protected HttpContext HttpContext
+          => base.ServiceContext.GetRequiredService<IHttpContextAccessor>().HttpContext!;
+
+        [AllowAnonymous]
+        public MyUser GetUser()
+        {
+            var user = (ClaimsPrincipal)base.ServiceContext.User;
+
+            return (user.Identity?.IsAuthenticated == true)
+                ? GetAuthenticatedUser(user)
+                : GetAnonymousUser();
+        }
+
+        [AllowAnonymous]
+        public async Task<MyUser> LoginAsync(string userName, string password, bool isPersistent, string customData)
+        {
+            if (ValidateCredentials(userName, password, customData))
+            {
+                // Maybe the claims is better extraced/created based on the "MyUser" type
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, userName),
+                    new Claim(ClaimTypes.Role, userName),
+                };
+
+                var principal = new ClaimsPrincipal(new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme));
+
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    principal,
+                    new AuthenticationProperties()
+                    {
+                        IsPersistent = isPersistent,
+                    });
+
+                return GetAuthenticatedUser(principal);
+            }
+            else
+            {
+                // login failure
+                return null;
+            }
+        }
+
+        [AllowAnonymous]
+        public async Task<MyUser> LogoutAsync()
+        {
+            await HttpContext.SignOutAsync();
+
+            return GetAnonymousUser();
+        }
+
+        // [RequiresAuthentication] -- not needed since we have Authorize on class
+        public void UpdateUser(MyUser user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private MyUser GetAuthenticatedUser(ClaimsPrincipal principal)
+        {
+            return new MyUser()
+            {
+                Name = principal.Identity.Name,
+                Roles = principal.FindAll(claim => claim.Type == ClaimTypes.Role).Select(claim => claim.Value).ToList(),
+            };
+        }
+
+        private static MyUser GetAnonymousUser()
+        {
+            return new MyUser() { Name = string.Empty, Roles = Array.Empty<string>() };
+        }
+
+        private static bool ValidateCredentials(string userName, string password, string customData)
+        {
+            return password == "password";
+        }
+
+        MyUser IAuthentication<MyUser>.Login(string userName, string password, bool isPersistent, string customData)
+            => throw new NotImplementedException("Async method is called at runtime");
+
+        MyUser IAuthentication<MyUser>.Logout()
+            => throw new NotImplementedException("Async method is called at runtime");
+    }
+}
