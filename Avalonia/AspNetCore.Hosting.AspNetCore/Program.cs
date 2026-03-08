1ee79174f643a15d75e3c8f860226ffc170bca9c
Daniel Svensson <Daniel-Svensson@users.noreply.github.com>
2026-03-08 20:58:26 +0100

    Update avalonia to fix WASM

--- Avalonia/AspNetCore.Hosting.AspNetCore/Program.cs
+++ Avalonia/AspNetCore.Hosting.AspNetCore/Program.cs
@@ -6,8 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Register AddOpenRiaServices and all DomainServices
 builder.Services.AddOpenRiaServices();
 builder.Services.AddTransient<SampleDomainService>();
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(policy =>
+	{
+		// ONLY for development, be more restrictive for production
+		policy.AllowAnyMethod();
+		policy.AllowAnyHeader();
+
+		policy.WithOrigins("https://localhost:7169", "http://localhost:5235");
+	});
+});
 
 var app = builder.Build();
+app.UseCors();
+
 app.MapGet("/", () => "Hello Open Ria Services !\n\nNow you can start the client and call the Sample service");
 
 // Enable mapping of all requests to root 
